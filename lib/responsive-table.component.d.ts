a774742f335fb4c65c3b0f6173b51be47224eb96
Daniel Ionut Hariga <Daniel.Hariga@Interautomation.de>
2023-12-19 07:26:23 +0100

    first-commit

---
+++ lib/responsive-table.component.d.ts
@@ -0,0 +1,5 @@
+import * as i0 from "@angular/core";
+export declare class ResponsiveTableComponent {
+    static ɵfac: i0.ɵɵFactoryDeclaration<ResponsiveTableComponent, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<ResponsiveTableComponent, "lib-responsive-table", never, {}, {}, never, never, false, never>;
+}
---
+++ public-api.d.ts
@@ -0,0 +1,3 @@
+export * from './lib/responsive-table.service';
+export * from './lib/responsive-table.component';
+export * from './lib/responsive-table.module';
